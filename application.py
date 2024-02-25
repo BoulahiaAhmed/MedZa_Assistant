51ac854ed37bb75ca2e758b2ebd9550e180b274b
Ahmed Khalil Boulahia <ahmedkhalilboulahia@gmail.com>
2024-02-25 11:25:12 +0100

    Add files via upload

---
+++ application.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from chatbot import Chatbot
+import markdown
+import random
+import time
+
+
+# Streamed response emulator
+def response_generator(response):
+    for word in response.split(" "):
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.title("MedZa Assistant")
+st.subheader("Always fresh, always informed!")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = Chatbot(prompt)
+        response = st.write_stream(response_generator(response))
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+st.sidebar.header("**Welcome to MedZa Assistant!**")
+st.sidebar.write("""\n\n I'm here to provide you with the latest updates, answer your burning questions.\n\n From trending news to real-time data, think of me as your knowledgeable sidekick, ready to assist you whenever you need it.\n\n Let's explore together and stay ahead of the curve!\n\n""")
+st.sidebar.divider()
+st.sidebar.write("""Created with :heart: by Ahmed & Hamza Boulahia""")
---
+++ chatbot.py
@@ -0,0 +1,95 @@
+from duckduckgo_search import DDGS
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-pro')  
+
+
+def DDGS_RAG(query, max_results=2):
+    with DDGS() as ddgs:
+        results = [r for r in ddgs.text(query, safesearch='on', timelimit='y', max_results=max_results)]
+
+    snippets = []
+    for result in results:
+        title = result['title']
+        body = result['body']
+        link = result['href']
+        snippet = {'title': title, 'body': body, 'link': link}
+        snippets.append(snippet)
+
+    return snippets
+
+
+def Gemini_generation(request, context):
+    prompt = f"""Your main task is to respond to the request using the context provided.
+    The context is indicated with the tag 'context' and the request is indicated with
+    the tag 'request'.
+    If the context doesn't have relevant information about the request, or if
+    the request is a basic and very simple one, you can then respond using your own
+    knowledge.
+    Here's some examples of a basic request: 'hello', 'whats 1+1?', 'who are you?'
+    If the knowledge you have contradicts the context provided. Then use the context
+    information.
+
+    Context:{context}
+    Request:{request}
+    """
+    response = model.generate_content(prompt,
+                                        generation_config=genai.types.GenerationConfig(
+                                        candidate_count=1,
+                                        max_output_tokens=500,
+                                        temperature=0.1))
+    return response
+
+
+def Gemini_digest(list_of_context, list_of_titles):
+    prompt = f""" Your task is to generate a text summary based on the provided data.
+    To complete this task successfully you need to follow these steps:
+
+    1- Generate separate summaries for each text in the provided list indicated with the tag 'list_of_context'.
+    2- Understand the context of each summary and generate separate summaries.
+    3- The final summary must be in bullet points, add a title for each bullet point, titles must be from the provided list indicated with the tag 'list_of_titles'.
+    4- Generate a good title for this complete and unified summary.
+
+    make sure the output is clear and easy to read.
+
+    list_of_context: {list_of_context}.
+    list_of_titles: {list_of_titles}.
+    """
+    response = model.generate_content(prompt,
+                                        generation_config=genai.types.GenerationConfig(
+                                        candidate_count=1,
+                                        max_output_tokens=500,
+                                        temperature=0.2))
+    return response
+
+
+def Chatbot(query):
+    # Call DDGS_RAG function to retrieve the data
+    source_data = DDGS_RAG(query)
+    answers=[]
+    links=[]
+    titles=[]
+    ref=[]
+
+    # Loop on the retrieved data and generate responses with Gemini_generation function
+    for data in source_data:
+        response = Gemini_generation(query, data['body'])
+        answers.append(response.text)
+        links.append(data['link'])
+        titles.append(data['title'])
+
+    # Reference creation (titles, link)
+    for i in range(len(links)):
+        ref.append(titles[i]+".  \nLink: "+links[i])
+
+    # Call the Gemini_digest function to return the final answer
+    summary = Gemini_digest(answers, titles)
+    ref_string = "\n\n".join(ref)
+    digest = f"""{summary.text}\n\nReferences:\n\n  {ref_string}"""
+    return digest
